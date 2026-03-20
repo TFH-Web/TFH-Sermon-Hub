f6bbeded0c72ca2d88115e49d50c4b9ace55e426
mocha8686 <june.mocha8686@gmail.com>
2026-03-19 17:58:49 -0700

    build: remove defaults

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "sanitize.css";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
-			<App />
+			<Routes>
+				<Route index element={<App />} />
+			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
 );
