a0c9590a77c1957701f5dd71ba9e2300ef919100
mocha8686 <june.mocha8686@gmail.com>
2026-03-19 21:37:04 -0700

    feat: sidebar title
    style: lint

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -1,10 +1,19 @@
 import Navbar from './Navbar';
 import './Sidebar.css';
+import { Link } from 'react-router';
+import logo from '$/assets/logo.png';
 
 export default function Sidebar() {
 	return (
 		<aside className="Sidebar">
-			<div className="Sidebar-logo">logo here</div>
+			<div className="Sidebar-title">
+				<Link className="Sidebar-titleLink" to="/">
+					<div className="Sidebar-logoContainer">
+						<img className="Sidebar-logo" src={logo} alt="TFH" />
+					</div>
+					<h1 className="Sidebar-header">Sermon Hub</h1>
+				</Link>
+			</div>
 			<Navbar />
 			<div className="Sidebar-profile">profiel here</div>
 		</aside>
