34db2c0c8f9a9e651cace9b69ba61a302061c1c0
Griffin <griffinjohnson1414@gmail.com>
2026-03-21 18:16:29 -0700

    fix: Added a placeholder component to FilterDropdown.tsx, fixed typos in Sermons.css and Sermons.tsx

--- Frontend/src/Sermons.tsx
+++ Frontend/src/Sermons.tsx
@@ -35,7 +35,7 @@ export default function Sermons() {
 	return (
 		<MainLayout title="Sermons">			
 			{/* Sermon Filter Buttons, clicks set as active and update the selectedSermonFilter state */}			
-			<h1><div className="sermon=tabs">
+			<h1><div className="sermon-tabs">
   				<button 
 					className={selectedSermonFilter === "All" ? "active-sermon-filter" : "sermon-tab"}
 					onClick={() => setSelectedSermonFilter("All")}>
--- Frontend/src/components/FilterDropdown.tsx
+++ Frontend/src/components/FilterDropdown.tsx
@@ -1,1 +1,5 @@
 {/* TODO: refactor these selects into reusable FilterDropdown component */}
+
+export default function FilterDropdown() {
+  return null;
+}
