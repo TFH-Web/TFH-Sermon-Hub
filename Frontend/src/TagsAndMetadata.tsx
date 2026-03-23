2f1069429118c758217098a0726bf5aa47b81ab5
M. Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2026-03-22 19:55:00 -0700

    Added "+ Add Tag" Button
    "+ Add Tag" button pulls up modal for adding tags. Adding tag does nothing yet but does check if anything at all was input for tag name. Button has no reaction to cursor hover or click yet.

--- Frontend/src/TagsAndMetadata.tsx
+++ Frontend/src/TagsAndMetadata.tsx
@@ -1,9 +1,61 @@
+import { useState }  from "react";
 import MainLayout from '$/components/MainLayout';
+import AddTagModal from '$/modals/AddTagModal';
 
 export default function TagsAndMetadata() {
+	const [addTagOpen, setAddTagOpen] = useState(false);
+
 	return (
 		<MainLayout title="Tags & Metadata">
-			<h2>Stinky cheese</h2>
+			<div 
+				style={{
+				display: 'flex',
+				justifyContent: 'left',
+				}}
+			>
+				
+			<h3 
+				style={{
+				textAlign: 'left'
+				}}
+			>
+				Tags & Metadata</h3>
+
+			<button
+				key={'addtag'}
+				className="Header-upload"
+				type="button"
+				onClick={() =>  setAddTagOpen(true)}
+				style={{
+					position: 'absolute',
+					right: 0,
+					height: '40px',
+					padding: '0 16px',
+					marginRight: '1.8rem',
+					marginTop: '0.5rem',
+					// borderRadius: '999px',
+					border: '1px solid #d8d8d8',
+					// background: 'addtag' ? '#7a9166' : '#ffffff',
+					// color: type === option.value ? '#ffffff' : '#363636',
+					background: '#5e6f55',
+					color: '#ffffff',
+					fontWeight: 600,
+					cursor: 'pointer',
+					}}
+			>
+				+ Add Tag
+							</button>
+
+			</div>
+
+		<AddTagModal
+			isOpen={addTagOpen}
+			onClose={() => setAddTagOpen(false)}
+		> 
+			</AddTagModal>
+
 		</MainLayout>
+
+
 	);
 }
