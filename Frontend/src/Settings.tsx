446c3bb6c3a17fabb06ff083b96cc88e4a5eb289
Nicole <nicolejoshuamiranda@csus.edu>
2026-04-06 16:21:33 -0700

    feat: add sub header & tab bar

--- Frontend/src/Settings.tsx
+++ Frontend/src/Settings.tsx
@@ -1,9 +1,22 @@
 import MainLayout from '$/components/MainLayout';
+import TabBar from '$/components/TabBar';
+import { useState } from 'react';
+
+
+const tabs = ['General', 'Authentication', 'AI Configuration', 'Security', 'Backup'];
 
 export default function Settings() {
+	const [activeTab, setActiveTab] = useState('General');
+
 	return (
 		<MainLayout title="Settings">
-			<h2>Stinky cheese</h2>
+			{/* Subheader */}
+			<div className="Settings-header">
+				<h2 className="Settings-heading">Settings</h2>
+			</div>
+
+			{/* Tab bar */}
+			<TabBar tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
 		</MainLayout>
 	);
 }
