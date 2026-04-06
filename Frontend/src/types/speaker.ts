c0deb2725c3e2b0a91f5465b4d6b8894e349af74
M. Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2026-04-06 05:18:31 -0700

    added type "Speaker"
    interface used to record: name, role, sermoncount, and profile icon color

---
+++ Frontend/src/types/speaker.ts
@@ -0,0 +1,7 @@
+/* This file defines the interface for the speaker data structures used in the Speakers component. It includes properties name, role, and sermoncount. */
+export interface Speaker {
+	name: string;
+	role: string;
+	sermoncount: string;
+	color: string;
+}
