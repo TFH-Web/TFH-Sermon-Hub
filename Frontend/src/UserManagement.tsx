b6c36375dffc22a7407030812ddff55ce51853bf
mocha8686 <june.mocha8686@gmail.com>
2026-04-07 10:03:12 -0700

    feat: info banner and add user button

--- Frontend/src/UserManagement.tsx
+++ Frontend/src/UserManagement.tsx
@@ -1,9 +1,21 @@
+import { Icon } from '@iconify-icon/react';
 import MainLayout from '$/components/MainLayout';
+import './UserManagement.css';
 
 export default function UserManagement() {
 	return (
 		<MainLayout title="User Management">
-			<h2>Stinky cheese</h2>
+			<header className="UserManagement-header">
+				<p className="Header-info">
+					Authentication is managed through Microsoft Entra SSO. All users must
+					sign in with their organizational Microsoft account.
+				</p>
+
+				<button type="button" className="Header-addUser u-button">
+					<Icon icon="lucide:plus" />
+					Add User
+				</button>
+			</header>
 		</MainLayout>
 	);
 }
