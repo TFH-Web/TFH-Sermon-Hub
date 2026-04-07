e4f206d93f97973588825dc962f83f9203ea141a
mocha8686 <june.mocha8686@gmail.com>
2026-04-07 12:31:55 -0700

    feat: table style

--- Frontend/src/UserManagement.tsx
+++ Frontend/src/UserManagement.tsx
@@ -1,8 +1,15 @@
 import { Icon } from '@iconify-icon/react';
 import MainLayout from '$/components/MainLayout';
 import './UserManagement.css';
-import { testUsers } from './data/users';
-import { canRemove, getFullName, getUser, userHue } from './types/user';
+import { testUsers } from '$/data/users';
+import {
+	canRemove,
+	dateRelative,
+	getFullName,
+	getInitials,
+	getUser,
+	userHue,
+} from '$/types/user';
 
 export default function UserManagement() {
 	const currentUser = getUser();
@@ -21,8 +28,8 @@ export default function UserManagement() {
 				</button>
 			</header>
 
-			<table className="userManagement-Users">
-				<thead>
+			<table className="UserManagement-users">
+				<thead className="Users-thead">
 					<tr>
 						<th>User</th>
 						<th>Email</th>
@@ -33,21 +40,38 @@ export default function UserManagement() {
 				</thead>
 				<tbody>
 					{testUsers.map(user => (
-						<tr
-							key={user.id}
-							style={{ '--h': userHue(user) } as React.CSSProperties}
-						>
-							<td>{getFullName(user)}</td>
-							<td>{user.email}</td>
-							<td>{user.role}</td>
-							<td>
+						<tr key={user.id}>
+							<td className="Users-name">
+								<p
+									className="Users-initials"
+									style={{ '--h': userHue(user) } as React.CSSProperties}
+								>
+									{getInitials(user)}
+								</p>
+								<p className="Users-fullName">{getFullName(user)}</p>
+							</td>
+							<td className="Users-email">{user.email}</td>
+							<td className="Users-role">
+								<p className={`Users-roleChip is-${user.role.toLowerCase()}`}>
+									{user.role}
+								</p>
+							</td>
+							<td className="Users-lastActive">
 								<time dateTime={user.lastActive.toISOString()}>
-									{user.lastActive.toUTCString()}
+									{dateRelative(user)}
 								</time>
 							</td>
-							<td>
-								<button type="button" className="u-button">Edit</button>
-								<button type="button" className="u-button" hidden={!canRemove(currentUser, user)}>Remove</button>
+							<td className="Users-actions">
+								<button type="button" className="Users-actionEdit u-button">
+									Edit
+								</button>
+								<button
+									type="button"
+									className="Users-actionRemove u-button"
+									hidden={!canRemove(currentUser, user)}
+								>
+									Remove
+								</button>
 							</td>
 						</tr>
 					))}
