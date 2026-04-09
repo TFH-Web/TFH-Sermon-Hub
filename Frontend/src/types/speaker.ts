fe342d09f9f9d126d52a60a7f4d18ef4a1e4867a
M. Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2026-04-09 10:46:13 -0700

    ran "npm run lint"

--- Frontend/src/types/speaker.ts
+++ Frontend/src/types/speaker.ts
@@ -1,8 +1,8 @@
 /* This file defines the interface for the speaker data structures used in the Speakers component. It includes properties name, role, and sermoncount. */
 export interface Speaker {
-  name: string;
-  role: string;
-  id: string;
-  sermoncount: string;
-  color: string;
+	name: string;
+	role: string;
+	id: string;
+	sermoncount: string;
+	color: string;
 }
