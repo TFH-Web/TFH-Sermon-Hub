8b5d9eb066e5ac2e76f46ff62adc120e6f1038a2
mocha8686 <june.mocha8686@gmail.com>
2026-04-21 19:37:58 -0700

    refactor: use Container for ImportActivity

--- Frontend/src/components/ImportActivity.tsx
+++ Frontend/src/components/ImportActivity.tsx
@@ -1,15 +1,16 @@
 import { dashboardImports } from '$/data/imports';
 import ImportItem from './ImportItem';
 import './ImportActivity.css';
+import Container from './Container';
 
 export default function ImportActivity() {
 	return (
-		<div className="ImportActivity">
+		<Container className="ImportActivity">
 			<h2 className="ImportActivity-title">Import Activity</h2>
 
 			{dashboardImports.map(job => (
 				<ImportItem key={job.id} job={job} />
 			))}
-		</div>
+		</Container>
 	);
 }
