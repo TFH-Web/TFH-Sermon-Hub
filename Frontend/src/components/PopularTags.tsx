88ddfe3576648fde57e34143b0f60cd52f0f21a0
mocha8686 <june.mocha8686@gmail.com>
2026-04-21 21:46:50 -0700

    refactor: use Tag in PopularTags

--- Frontend/src/components/PopularTags.tsx
+++ Frontend/src/components/PopularTags.tsx
@@ -1,6 +1,7 @@
 import './PopularTags.css';
 import { tags } from '$/data/tags';
 import Container from './Container';
+import Tag from './Tag';
 
 export default function PopularTags() {
 	return (
@@ -8,13 +9,15 @@ export default function PopularTags() {
 			<h3 className="PopularTags-title">Popular Tags</h3>
 			<div className="PopularTags-list">
 				{tags.map(tag => (
-					<a
+					<Tag
+						as="a"
+						variant="solid"
 						key={tag.name}
 						href={`/tags?tag=${tag.name}`}
-						className="PopularTags-pill u-button"
+						className="PopularTags-tag u-button"
 					>
 						{tag.name} ({tag.count})
-					</a>
+					</Tag>
 				))}
 			</div>
 		</Container>
--- Frontend/src/components/Tag.tsx
+++ Frontend/src/components/Tag.tsx
@@ -2,19 +2,23 @@ import type React from 'react';
 import './Tag.css';
 import clsx from 'clsx';
 
-export interface TagProps {
-	variant?: 'green' | 'outline' | 'red' | 'amber' | 'blue' | 'admin';
+export type TagProps<Component extends React.ElementType> =  {
+	variant?: 'green' | 'outline' | 'red' | 'amber' | 'blue' | 'admin' | 'solid';
 	className?: string;
-}
+	as?: Component;
+} & React.ComponentPropsWithoutRef<Component>;
 
-export default function Tag({
+export default function Tag<Component extends React.ElementType = "span">({
 	variant = 'green',
 	children,
 	className,
-}: React.PropsWithChildren<TagProps>) {
+	as,
+	...props
+}: React.PropsWithChildren<TagProps<Component>>) {
+	const Component = as ?? 'span';
 	return (
-		<span className={clsx('Tag', `Tag--${variant}`, className)}>
+		<Component className={clsx('Tag', `Tag--${variant}`, className)} {...props}>
 			{children}
-		</span>
+		</Component>
 	);
 }
