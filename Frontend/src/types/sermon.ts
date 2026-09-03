99f70207649ace8870419e5140196f2b87b91fee
mocha8686 <june.mocha8686@gmail.com>
2026-09-02 19:36:01 -0700

    refactor: add Zod schemas for series and sermons

--- Frontend/src/types/sermon.ts
+++ Frontend/src/types/sermon.ts
@@ -1,19 +1,25 @@
 import type { TagVariant } from '$/components/Tag';
+import z from 'zod';
+import { Series } from './series';
 
 /* This file defines the interface for the sermon data structures used in the Sermons component. It includes properties such as title, speaker, series, date, time, and tags. */
 export const statuses = ['Published', 'Processing', 'Draft', 'Failed'] as const;
-export type Status = (typeof statuses)[number];
+export const Status = z.enum(statuses);
+export type Status = z.infer<typeof Status>;
 
-export interface Sermon {
-	id: number;
-	title: string;
-	speaker: string;
-	series: string | null;
-	date: Date;
-	tags: string[];
-	status: Status;
-	duration: number;
-}
+export const Sermon = z.object(
+	{
+		id: z.number(),
+		title: z.string(),
+		speaker: z.string(),
+		series: Series.nullish(),
+		date: z.coerce.date(),
+		tags: z.string().array(),
+		status: Status,
+		duration: z.number().nonnegative(),
+	}
+)
+export type Sermon = z.infer<typeof Sermon>;
 
 export function linkTo(sermon: Sermon): string {
 	return `/sermons/${sermon.id}`;
