874ca7b007c7839b1eec279aaadb85e552d9a1c2
mocha8686 <june.mocha8686@gmail.com>
2026-09-03 13:04:18 -0700

    fix: show temporary error/loading states

--- Frontend/src/Sermons.tsx
+++ Frontend/src/Sermons.tsx
@@ -67,10 +67,10 @@ export default function Sermons() {
 	const [sortCategory, setSortCategory] = useState<SortCategory>('Newest');
 
 	// TODO: error state at error
-	if (query.isError) return;
+	if (query.isError) return <MainLayout title="Sermons" className="Sermons"><h1>Error!</h1></MainLayout>;
 
 	// TODO: loading state while pending
-	if (query.isPending) return;
+	if (query.isPending) return <MainLayout title="Sermons" className="Sermons"><h1>Loading...</h1></MainLayout>;
 
 	return (
 		<MainLayout title="Sermons" className="Sermons">
