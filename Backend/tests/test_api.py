0cbc3a4e4a5c5d94174b6e8159cd34ef26e6fe15
mocha8686 <june.mocha8686@gmail.com>
2026-09-03 15:37:57 -0700

    fix: add /api prefix to pytest requests

--- Backend/tests/test_api.py
+++ Backend/tests/test_api.py
@@ -17,12 +17,12 @@ from tsh.schemas import (
 def test_series(client: FlaskClient, snapshot: Snapshot):
     from tsh.models import Series
 
-    res = client.get("/series")
+    res = client.get("/api/series")
     seriess: list[Series] = seriess_schema.loads(res.data)
     assert seriess == snapshot
 
     for series in seriess:
-        res = client.get(f"/series/{series.id}")
+        res = client.get(f"/api/series/{series.id}")
         res_series: Series = series_schema.loads(res.data)
         assert res_series == series
 
@@ -30,12 +30,12 @@ def test_series(client: FlaskClient, snapshot: Snapshot):
 def test_speakers(client, snapshot):
     from tsh.models import Speaker
 
-    res = client.get("/speakers")
+    res = client.get("/api/speakers")
     speakers: list[Speaker] = speakers_schema.loads(res.data)
     assert speakers == snapshot
 
     for speaker in speakers:
-        res = client.get(f"/speakers/{speaker.id}")
+        res = client.get(f"/api/speakers/{speaker.id}")
         res_speaker: Speaker = speaker_schema.loads(res.data)
         assert res_speaker == speaker
 
@@ -43,23 +43,23 @@ def test_speakers(client, snapshot):
 def test_sermons(client, snapshot):
     from tsh.models import Sermon
 
-    res = client.get("/sermons")
+    res = client.get("/api/sermons")
     sermons: list[Sermon] = sermons_schema.loads(res.data)
     assert sermons == snapshot
 
     for sermon in sermons:
-        res = client.get(f"/sermons/{sermon.id}")
+        res = client.get(f"/api/sermons/{sermon.id}")
         res_sermon: Sermon = sermon_schema.loads(res.data())
         assert res_sermon == sermon
 
 
 def test_get_id_404(client):
-    paths = [
-        "/series",
-        "/speakers",
-        "/sermons",
+    slugs = [
+        "series",
+        "speakers",
+        "sermons",
     ]
 
-    for path in paths:
-        res = client.get(f"{path}/1000")
+    for slug in slugs:
+        res = client.get(f"/api/{slug}/10000")
         assert res.status == "404 NOT FOUND"
