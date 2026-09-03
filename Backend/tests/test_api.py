5046306639660ae2d10e0a7c72795d4d716bdde4
mocha8686 <june.mocha8686@gmail.com>
2026-09-03 13:30:36 -0700

    refactor: use marshmallow schemas for deserialziation in tests

--- Backend/tests/test_api.py
+++ Backend/tests/test_api.py
@@ -1,17 +1,29 @@
-import pytest  # noqa: F401
 import json
 
+import pytest  # noqa: F401
+from flask.testing import FlaskClient
+from syrupy.data import Snapshot
 
-def test_series(client, snapshot):
+from tsh.schemas import (
+    series_schema,
+    seriess_schema,
+    sermon_schema,
+    sermons_schema,
+    speaker_schema,
+    speakers_schema,
+)
+
+
+def test_series(client: FlaskClient, snapshot: Snapshot):
     from tsh.models import Series
 
     res = client.get("/series")
-    series = [Series(**dict) for dict in json.loads(res.data)]
-    assert series == snapshot
+    seriess: list[Series] = seriess_schema.loads(res.data)
+    assert seriess == snapshot
 
-    for series in series:
+    for series in seriess:
         res = client.get(f"/series/{series.id}")
-        res_series = Series(**json.loads(res.data))
+        res_series: Series = series_schema.loads(res.data)
         assert res_series == series
 
 
@@ -19,35 +31,35 @@ def test_speakers(client, snapshot):
     from tsh.models import Speaker
 
     res = client.get("/speakers")
-    speakers = [Speaker(**dict) for dict in json.loads(res.data)]
+    speakers: list[Speaker] = speakers_schema.loads(res.data)
     assert speakers == snapshot
 
     for speaker in speakers:
         res = client.get(f"/speakers/{speaker.id}")
-        res_speakers = Speaker(**json.loads(res.data))
-        assert res_speakers == speaker
+        res_speaker: Speaker = speaker_schema.loads(res.data)
+        assert res_speaker == speaker
 
 
 def test_sermons(client, snapshot):
     from tsh.models import Sermon
 
     res = client.get("/sermons")
-    sermons = [Sermon(**dict) for dict in json.loads(res.data)]
+    sermons: list[Sermon] = sermons_schema.loads(res.data)
     assert sermons == snapshot
 
     for sermon in sermons:
         res = client.get(f"/sermons/{sermon.id}")
-        res_sermon = Sermon(**json.loads(res.data))
+        res_sermon: Sermon = sermon_schema.loads(res.data())
         assert res_sermon == sermon
 
 
 def test_get_id_404(client):
     paths = [
-        '/series',
-        '/speakers',
-        '/sermons',
+        "/series",
+        "/speakers",
+        "/sermons",
     ]
 
     for path in paths:
-        res = client.get(f'{path}/1000')
-        assert res.status == '404 NOT FOUND'
+        res = client.get(f"{path}/1000")
+        assert res.status == "404 NOT FOUND"
